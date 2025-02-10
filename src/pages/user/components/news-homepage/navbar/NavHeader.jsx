87b2af1a0011b49fffd91cf273efcfefb5df12ea
Refanz <refandasuryasaputra@gmail.com>
2025-02-11 04:38:25 +0700

    fix responsive on landing page

--- src/pages/user/components/news-homepage/navbar/NavHeader.jsx
+++ src/pages/user/components/news-homepage/navbar/NavHeader.jsx
@@ -11,11 +11,11 @@ NavHeader.propTypes = {
 function NavHeader(props) {
     return (
         <div className="navbar bg-base-100 max-w-screen-xl">
-            <div className="navbar-start w-full gap-2">
+            <div className="navbar-start  gap-2">
                 <div className="border-r-2 border-r-black px-2">
-                    <a className="text-xl font-bold text-red-600">EnigmaNews</a>
+                    <a className="text-xl font-bold text-red-600 cursor-pointer" onClick={() => props.onChangePage("home")}>EnigmaNews</a>
                 </div>
-                <div className="flex w-full items-center gap-2 md:visible invisible">
+                <div className="flex items-center gap-2 md:visible invisible">
                     {
                         categories.map((category, index) => {
                             return (
@@ -26,22 +26,21 @@ function NavHeader(props) {
                         })
                     }
                 </div>
-
-                <div className="dropdown invisible">
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                        <ListBulletIcon className="w-6"/>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
-                    </ul>
-                </div>
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end md:flex hidden">
                 <button className="btn btn-error text-white" onClick={() => props.onChangePage("login")}>Login</button>
+            </div>
+            <div className="navbar-end dropdown md:hidden flex">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                    <ListBulletIcon className="w-6"/>
+                </div>
+                <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                    <li><a>Homepage</a></li>
+                    <li><a>Portfolio</a></li>
+                    <li><a>About</a></li>
+                </ul>
             </div>
         </div>
     );
